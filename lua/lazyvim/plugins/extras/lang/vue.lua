f815fe0d12e82d4395f2bb93e695422d38ca94e6
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2024-04-11 19:27:27 +0900

    feat(vue): add vue support

---
+++ lua/lazyvim/plugins/extras/lang/vue.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "vue",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        tsserver = function(_, opts)
+          local mason_registry = require("mason-registry")
+          local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+            .. "/node_modules/@vue/language-server"
+
+          opts.init_options = {
+            plugins = {
+              {
+                name = "@vue/typescript-plugin",
+                location = vue_language_server_path,
+                languages = { "vue", "typescript" },
+              },
+            },
+          }
+          opts.filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+        end,
+      },
+      servers = {
+        volar = {},
+        tsserver = {},
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "marilari88/neotest-vitest",
+    },
+    opts = {
+      adapters = {
+        ["neotest-vitest"] = {},
+      },
+    },
+  },
+}
